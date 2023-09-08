d994174ac5df6fadd1f7e63097350302c71b46a3
FullStackSimon <fullstacksimon@gmail.com>
2023-09-08 12:46:06 +0200

    added moving ground

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from pygame.locals import *
 
 pygame.init()
 
+# Init Clock
+clock = pygame.time.Clock()
+fps = 60
+
+# Define Screen Size
 screen_width = 864
 screen_height = 936
 
@@ -11,12 +16,26 @@ pygame.display.set_caption('Flappy 911')
 
 # Load Images
 bg = pygame.image.load('img/bg.png')
+ground = pygame.image.load('img/ground.png')
+
+# Define Variables
+ground_scroll = 0
+scroll_speed = 4
 
 
+# Main Game Loop
 run = True
 while run:
-    screen.blit(bg, (0,0))
 
+    # Run Clock with predifend FPS
+    clock.tick(fps)
+
+    # Render Images on Screen
+    screen.blit(bg, (0,0))
+    screen.blit(ground, (ground_scroll, 768))
+    ground_scroll -= scroll_speed 
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
