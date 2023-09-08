6222ba126e3dee3ecaf08c4dcb0383fc5ffdea8d
FullStackSimon <fullstacksimon@gmail.com>
2023-09-08 13:31:46 +0200

    added animated bird

--- main.py
+++ main.py
@@ -18,6 +18,36 @@ pygame.display.set_caption('Flappy 911')
 bg = pygame.image.load('img/bg.png')
 ground = pygame.image.load('img/ground.png')
 
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+    def update(self):
+        # Handle Animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index > 2:
+                self.index = 0
+        self.image = self.images[self.index]
+
+
+plane_group = pygame.sprite.Group()
+
+flappy = Plane(100, int(screen_height / 2))
+plane_group.add(flappy)
+
 # Define Variables
 ground_scroll = 0
 scroll_speed = 4
@@ -30,8 +60,15 @@ while run:
     # Run Clock with predifend FPS
     clock.tick(fps)
 
-    # Render Images on Screen
+    ### Render Images on Screen
+    # Render Background
     screen.blit(bg, (0,0))
+
+    # Render Plane on BG
+    plane_group.draw(screen)
+    plane_group.update()
+
+    # Render Ground & Scroll BG
     screen.blit(ground, (ground_scroll, 768))
     ground_scroll -= scroll_speed 
     if abs(ground_scroll) > 35:
