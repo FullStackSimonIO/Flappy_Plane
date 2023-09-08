83e59679bef8b7d7b844cb862f109e27bd09bbf2
FullStackSimon <fullstacksimon@gmail.com>
2023-09-08 15:51:20 +0200

    added collision detection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 
 
@@ -25,12 +26,15 @@ ground_scroll = 0
 scroll_speed = 4
 flying = False
 game_over = False
+skyskraper_gap = 150
+skyskraper_frequency = 1500 # in milliseconds
+last_skyskraper = pygame.time.get_ticks() - skyskraper_frequency
 
 
 
 # Initialize Screen
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Flappy 911')
+pygame.display.set_caption('Flappy Terrorists')
 
 
 
@@ -42,6 +46,7 @@ ground = pygame.image.load('img/ground.png')
 
 # Define Plane Class 
 class Plane(pygame.sprite.Sprite):
+
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
@@ -57,6 +62,7 @@ class Plane(pygame.sprite.Sprite):
 
     # Define Update Function
     def update(self):
+
         if flying == True:
             self.vel += 0.5
             if self.vel > 8:
@@ -71,9 +77,10 @@ class Plane(pygame.sprite.Sprite):
                 self.vel = -10
             if pygame.mouse.get_pressed()[0] == 0:
                 self.clicked = False
+
             # Handle Animation
-            self.counter += 1
             flap_cooldown = 5
+            self.counter += 1
 
             if self.counter > flap_cooldown:
                 self.counter = 0
@@ -88,10 +95,33 @@ class Plane(pygame.sprite.Sprite):
             self.image = pygame.transform.rotate(self.images[self.index], -90)
 
 
+class Skyscrapers(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        # Position One is from Top, Position 2 from Bottom
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(skyskraper_gap / 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + int(skyskraper_gap / 2)]
+    
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
+
 plane_group = pygame.sprite.Group()
+skyskraper_group = pygame.sprite.Group()
 
 flappy = Plane(100, int(screen_height / 2))
+
 plane_group.add(flappy)
+
+
 
 # Main Game Loop
 run = True
@@ -106,18 +136,40 @@ while run:
 
     # Render Plane on BG
     plane_group.draw(screen)
+    skyskraper_group.draw(screen)
     plane_group.update()
 
+    screen.blit(ground, (ground_scroll, 768))
+
+    # Check for Collision
+    if pygame.sprite.groupcollide(plane_group, skyskraper_group, False, False) or flappy.rect.top < 0:
+        game_over = True
+
+
+
     # Check if Bird has hit ground
-    if flappy.rect.bottom > 768:
+    if flappy.rect.bottom >= 768:
         game_over = True
         flying = False
     
-    if game_over == False:
-        screen.blit(ground, (ground_scroll, 768))
+    if flying == True and game_over == False:
+        # Generate new pipes
+        time_now = pygame.time.get_ticks()
+        if time_now - last_skyskraper > skyskraper_frequency:
+            skyskraper_height = random.randint(-100, 100)
+            btm_skyskraper = Skyscrapers(screen_width, int(screen_height / 2) + skyskraper_height, -1)
+            top_skyskraper = Skyscrapers(screen_width, int(screen_height / 2) + skyskraper_height, 1)
+            skyskraper_group.add(btm_skyskraper)
+            skyskraper_group.add(top_skyskraper)
+            last_skyskraper = time_now
+        
+        skyskraper_group.update()
+
         ground_scroll -= scroll_speed 
         if abs(ground_scroll) > 35:
             ground_scroll = 0
+        
+        skyskraper_group.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
